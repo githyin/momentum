32dd623042b643952069d5af1c9911a3706746d9
githyin <kmkm9460@gmail.com>
2023-12-12 09:28:07 +0900

    make login

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-alert('hi')
+const loginForm = document.querySelector(".login-form")
+const loginInput = document.querySelector(".login-form input")
+const greeting = document.querySelector(".greeting")
+
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "userName"
+
+function onLoginSubmit(event){
+    event.preventDefault()
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    const userName = loginInput.value
+    localStorage.setItem(USERNAME_KEY, userName)
+    paintGreetings(savedUsername)
+}
+
+function paintGreetings(userName){
+    greeting.innerText = `Hello ${userName}`
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+
+if(savedUsername ===null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.addEventListener("submit", onLoginSubmit)
+} else {
+    paintGreetings(savedUsername)
+}
